7075b526fcf9a6e725d8cd038e42ea95c69f1eb2
DavidYop <david@david.fr>
2019-04-04 12:51:35 +0200

    First Commit Web CV David PETROVIC

---
+++ Web-CV - David-PETROVIC/js/script.js
@@ -0,0 +1,67 @@
+// Animation scroll 
+$(function(){
+	$("#btnPlus, .navbar a, footer a").on("click", function(event){		
+		event.preventDefault();
+		const hash = this.hash;		
+		$('body, html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})		
+	});	
+
+
+//Fermer la navbar collapse on click
+	$(document).on('click',function(){
+		$('.collapse').collapse('hide');
+	})
+
+
+//Tooltips
+	$('[data-toggle="tooltip"]').tooltip({trigger : "hover"});
+
+
+// Afficher plus d'infos
+	$("#divInfo").height(62);
+	$("#btnPlus").click(function(){
+	if ($("#divInfo").height() == 62){
+		$("#btnPlus").tooltip("hide").attr("data-original-title", "Afficher moins d'informations")
+		.css("transform", "rotate(180deg)").html("<i class='fas fa-minus fa-2x'></i>");
+		$("#divInfo").css("height", "100%");
+		const hauteur = $("#divInfo").height();
+		$("#divInfo").css("height", "62px");
+		$("#motiv").animate({color: "#fff"}, 1000);
+		$("#divInfo").animate({height: hauteur}, 1000);
+		$("#shadow").animate({boxShadow: "none"});
+	} else {
+		$("#btnPlus").tooltip("hide").attr("data-original-title", "Afficher plus d'informations")
+		.css("transform", "rotate(90deg)").html("<i class='fas fa-plus fa-2x'></i>");
+		$("#motiv").animate({color: "#9DA8B5"}, 1000);
+		$("#divInfo").animate({height: "62px"}, 1000);
+		$("#shadow").animate({boxShadow: "0 -15px 14px -4px #fff"});
+		}
+	});
+
+	
+//Formulaire Contact
+    $("#contact-form").submit(function(e){
+        e.preventDefault();
+        $(".msgErreur").empty();
+        const postdata = $("#contact-form").serialize();
+        $.ajax({
+            type: "POST",
+            url: "Ressources/php/contact.php",
+            data: postdata,
+            dataType: "json",
+            success: function(json){
+                if(json.isSuccess){
+					$("#contact-form")[0].reset();
+                    $(".msgSend").css("visibility", "visible");
+                } else {
+                    $("#prenom + .msgErreur").html(json.prenomError);
+                    $("#nom + .msgErreur").html(json.nomError);
+                    $("#email + .msgErreur").html(json.emailError);
+                    $("#phone + .msgErreur").html(json.phoneError);
+                    $("#message + .msgErreur").html(json.messageError);
+                }
+            }
+        });
+	});
+
+});
---
+++ Web-CV - David-PETROVIC/php/contact.php
@@ -0,0 +1,76 @@
+<?php 
+
+
+    if(empty($_POST)){
+        header('Location: ../../index.html');
+    }
+
+    $array = array("prenomError" => "", "nomError" => "", "emailError" => "", "phoneError" => "", "messageError" => "", "isSuccess" => true);
+
+    $emailTo = "monmail@mondomaine.fr";
+    $prenom = verifyInput($_POST["prenom"]);
+    $nom = verifyInput($_POST["nom"]);
+    $email = verifyInput($_POST["email"]);
+    $phone = verifyInput($_POST["phone"]);
+    $message = verifyInput($_POST["message"]);
+    $emailText = "";
+
+    if(empty($prenom)){
+        $array["prenomError"] = "Merci de renseigner votre prénom.";
+        $array["isSuccess"] = false;
+    } else {
+        if(!preg_match('/^[a-zA-Z- ]+$/', $prenom)) {
+            $array["prenomError"] = "Votre prénom doit être composé de lettres.";
+            $array["isSuccess"] = false;
+        }
+        $emailText .= "Prénom : {$prenom}\n";
+    }
+
+    if(empty($nom)){
+        $array["nomError"] = "Merci de renseigner votre nom.";
+        $array["isSuccess"] = false;
+    }  else {
+        if(!preg_match('/^[a-zA-Z- ]+$/', $nom)) {
+            $array["nomError"] = "Votre nom doit être composé de lettres.";
+            $array["isSuccess"] = false;
+        }
+        $emailText .= "Nom : {$nom}\n";
+    }
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $array["emailError"] = "L'adresse e-mail n'est pas correcte.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Email : {$email}\n";
+    }
+
+    if(!preg_match("/^[0-9 .]*$/", $phone)){
+        $array["phoneError"] = "Le numéro doit être composé de chiffres.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Numéro de téléphone : {$phone}\n";
+    }
+
+    if(empty($message)){
+        $array["messageError"] = "Merci de compléter le corps de votre message.";
+        $array["isSuccess"] = false;
+    } else {
+        $emailText .= "Message : {$message}\n";
+    }
+
+    if($array["isSuccess"]){
+        $headers= "From: {$prenom} {$nom} <{$email}>\r\nReply-To: {$email}";
+        mail($emailTo, "Nouveau message WEB CV", $emailText, $headers);
+    }
+
+    echo json_encode($array);
+
+    function verifyInput($var){
+        $var = trim($var);
+        $var = stripslashes($var);
+        $var = htmlspecialchars($var);
+        return $var;
+    }
+    
+
+?>
